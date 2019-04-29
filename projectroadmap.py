22065d042aaef3801d8054fb52c38a24132d32bc
sreed56 <47984415+sreed56@users.noreply.github.com>
2019-04-29 11:42:43 -0400

    Add files via upload

---
+++ projectroadmap.py
@@ -0,0 +1,11 @@
+#This is a list of steps for operating the raspberry pi security camera.
+- [X] Gather raspberry pi 3b, motion sensor, and camera.
+- [X] Properly connect the motion sensor and camera to the raspberry pi board.
+- [X] import Gpiozero, motion sensor, and picamera files into code.
+- [X] import date and time of motion capture into the code.
+- [ ] Display image of motion capture with proper recording of date and time.
+#Below is a list of steps that could protect the security of the footage caught.
+- [ ] Develop a secure login code for the user when footage is captured.
+- [ ] Import the secure login code to the initial security code.
+- [ ] Develop a website that can hold each piece of footage captured.
+
